dbd2c01b3f413c36c15ff20faed894c3b0d1bdf3
mac <dmitri-shadri@yandex.ru>
2021-08-22 15:03:49 +0300

    Внес изменения

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,9 @@
   public class CreditPaymentService {
-    public double calculate(double Rate) {
-        double creditAmount = 1_000_000;
-        int creditTerm = 12;
-        double monthlyInterest = Rate / 100 / creditTerm;
-        double degree = Math.pow(1 + monthlyInterest, 12);
-    return creditAmount * (monthlyInterest +(monthlyInterest / (degree - 1)));
-
-    }
-    }
+      public double calculate(int creditAmount, int creditTerm, double interestRate) {
+          double monthlyInterest = interestRate / 100 / 12;
+          double degree = monthlyInterest / (Math.pow(1 + monthlyInterest, creditTerm) -1);
+          double MonthlyPayment = creditAmount * (monthlyInterest + degree);
+          double result = Math.round(MonthlyPayment);
+          return result;
+      }
+  }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main        {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double interestRate = 9.99;
-        double MonthlyPayment = service.calculate(interestRate);
+        double MonthlyPayment = service.calculate(1_000_000, 12, 9.99);
+        // CreditTerm = срок кредита в месяцах
         System.out.println("Ежемесячный платёж " + MonthlyPayment);
         System.out.println("Процентная ставка " + interestRate);
 
             }
         }
-
